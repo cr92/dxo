79fb7890af912823d8509e9541f84fb0f00382e7
cr92 <agni_18_roy@yahoo.com>
2017-01-10 06:41:30 +0530

    insert in db func added

--- routes/route1.js
+++ routes/route1.js
@@ -1,32 +1,40 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root123',
-  database : 'images'
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: 'images'
 });
 
+var data = { album_id: 'jhfjhfhkf', sources: ['fgfg', 'fuhfi', 'ydyu'] };
+insertInDb(data);
 
 
 
-connection.connect();
-var album_name='hello';
-var query_string='SELECT stringified_src from images_table where album_id="hellow"';
-console.log(query_string)
-connection.query(query_string, function(err, rows, fields) 
-{
-  if (err) throw err;
-  console.log(rows);
-  console.log('The solution is: ', rows[0]['stringified_src']);
-});
-
-connection.end();
 
 
-function insertInDb(data,callback)
-{
-	var album_id=data['album_id'];
-	var sources=JSON.stringify(data['sources']);
 
-	var insert_query='INSERT INTO images_table (album_id, stringified_src) VALUES ("hooha","fyy")';
+
+
+
+
+
+function insertInDb(data) {
+    connection.connect();
+    var album_id = data['album_id'];
+    var sources = JSON.stringify(data['sources']);
+
+    var data_to_db = { album_id: album_id, stringified_src: sources };
+
+    console.log(data_to_db);
+
+    connection.query('INSERT INTO images_table SET ?', data_to_db, function(error, result) {
+        if (error) {
+            console.log(error);
+            connection.end();
+        } else {
+            console.log('success' + result);
+            connection.end();
+        }
+    });
 }
