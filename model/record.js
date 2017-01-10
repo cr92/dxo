63d169d1e4d226cb5593081c9504e81a97e491c1
cr92 <agni_18_roy@yahoo.com>
2017-01-10 14:05:52 +0530

    model removed

--- model/record.js
+++ model/record.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
 var albumSchema = mongoose.Schema({
     albumname: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
 });
